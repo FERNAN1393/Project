5bc906b9cc489094bc59f24331546262606f9516
FERNAN1393 <42189476+FERNAN1393@users.noreply.github.com>
2018-09-06 11:52:22 -0500

    Add files via upload

---
+++ SGAIJE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SGAIJE.Startup))]
+namespace SGAIJE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
